e27f164716d3851c4598a0234fd8167fb445c770
Pedro Vinícius P. Dias <pedro.dias@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2023-03-23 12:43:35 -0300

    Contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -11,40 +11,49 @@ export default function Contact() {
                 container 
                 xl 
                 grid grid-cols-2 place-content-center gap-20
-                p-10
-                shadow
+                border-solid border border-gray-100
+                m-auto
+                mt-16
+                rounded-lg
+                antialiased
+                font-sans
         '>
-            <div>
+            <div className='
+                grid gap-10
+                px-10 py-20
+                bg-gradient-to-bl from-white to-gray-100
+                border-r border-gray-100
+            '>
                 <h1 className='
                     text-2xl
                     font-bold
                 '>
                     Get in touch
                 </h1>
-                <p>
+                <p className='text-gray-500'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure itaque nulla aut
                     consequuntur magni esse velit maxime rem, vitae explicabo accusantium, 
                     reiciendis similique ratione animi delectus laboriosam mollitia quibusdam ut.
                 </p>
 
-                <div>
-                    <div>
-                        <BuildingOffice2Icon />
+                <div className='text-gray-500 text-sm grid gap-5'>
+                    <div className='flex gap-3 items-start'> 
+                        <BuildingOffice2Icon className='text-gray-400' />
                         <div>
                             545 Mavis Island<br/>
                             Chicago, IL 99191
                         </div>
                     </div>
 
-                    <div>
-                        <PhoneIcon />
+                    <div className='flex gap-3 items-start'>
+                        <PhoneIcon className='text-gray-400' />
                         <div>
                             +1 (555) 234-5678
                         </div>
                     </div>
 
-                    <div>
-                        <EnvelopeIcon />
+                    <div className='flex gap-3 items-start'>
+                        <EnvelopeIcon className='text-gray-400' />
                         <div>
                             contact@exemple.com
                         </div>
@@ -52,7 +61,112 @@ export default function Contact() {
                 </div>
             </div>
 
-            <form action=""></form>
+            <form
+                className='
+                    flex flex-wrap gap-8
+                    py-20 px-8
+                ' 
+                action=""
+            >
+                <div className='grid gap-3 h-fit flex-1'>
+                    <label 
+                        htmlFor=""
+                        className='font-bold text-sm'
+                    >
+                        First name
+                    </label>
+                    <input
+                        className='
+                        border-2 border-gray-200 border-solid rounded-md 
+                        px-3 py-1
+                        h-10
+                        w-full
+                        '
+                        type="text" 
+                    />
+                </div>
+
+                <div className='grid gap-3 h-fit flex-1'>
+                    <label 
+                        htmlFor=""
+                        className='font-bold text-sm'
+                    >
+                        Last name
+                    </label>
+                    <input
+                        className='
+                        border-2 border-gray-200 border-solid rounded-md 
+                        px-3 py-1
+                        h-10
+                        w-full
+                        '
+                        type="text" 
+                    />
+                </div>
+                
+                <div className='grid gap-3 h-fit w-full'>
+                    <label 
+                        htmlFor=""
+                        className='font-bold text-sm'
+                    >
+                        E-mail
+                    </label>
+                    <input
+                        className='
+                        border-2 border-gray-200 border-solid rounded-md
+                        px-3 py-1
+                        h-10
+                        w-full
+                        '
+                        type="text" 
+                    />
+                </div>
+
+                <div className='grid gap-3 h-fit w-full'>
+                    <label 
+                        htmlFor=""
+                        className='font-bold text-sm'
+                    >
+                        Phone number
+                    </label>
+                    <input
+                        className='
+                        border-2 border-gray-200 border-solid rounded-md 
+                        px-3 py-1
+                        h-10
+                        w-full
+                        '
+                        type="text" 
+                    />
+                </div>
+
+                <div className='grid gap-3 h-fit w-full'>
+                    <label 
+                        htmlFor=""
+                        className='font-bold text-sm'
+                    >
+                        Message
+                    </label>
+                    <textarea
+                        className='
+                        border-2 border-gray-200 border-solid rounded-md 
+                        px-3 py-1
+                        h-20
+                        w-full
+                        '
+                        type="text" 
+                    ></textarea>
+                </div>
+
+                <button
+                    className='bg-indigo-600 p-3 rounded-md
+                        font-bold text-white text-sm mr-0 ml-auto
+                    '
+                >
+                    Send message
+                </button>
+
+            </form>
 
             
         </div>
